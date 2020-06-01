7017cad3b981d478b651fc4b6a676eb0cb9e18eb
kouta <darkfm@vera.com.uy>
2020-05-31 21:56:22 -0300

    shell para emscripten y compilar shell

---
+++ emscr/compile_shell.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+hashes = $(git log --pretty=format:"<li>%h - %s</li><br/>" | tr -d '\n')
+latest_hash = $(git log --pretty=format:"%h" | head -1)
+
+cat shell.html | sed "s/%VERSIONINFO%/$hashes/g" | sed "s/%LATESTCOMMIT%/$latest_hash/g" > compiled_shell.html
